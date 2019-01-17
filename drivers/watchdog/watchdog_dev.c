9197d985d06724769cfb2a9e4dde975d758a6e51
acaccia <acaccia@ulb.ac.be>
2019-01-17 16:41:23 +0100

    [enh] completely remove watchdog message

--- drivers/watchdog/watchdog_dev.c
+++ drivers/watchdog/watchdog_dev.c
@@ -848,7 +848,6 @@ static int watchdog_release(struct inode *inode, struct file *file)
 
 	/* If the watchdog was not stopped, send a keepalive ping */
 	if (err < 0) {
-		pr_info("watchdog%d: watchdog did not stop!\n", wdd->id);
 		watchdog_ping(wdd);
 	}
 
